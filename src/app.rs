d266a548159a03512475bbb6d7d71ece1dfa51e5
8uff3r <8uff3r@gmail.com>
2025-03-02 15:56:21 +0330

    Improve styles

--- src/app.rs
+++ src/app.rs
@@ -40,10 +40,10 @@ fn Home() -> impl IntoView {
 
     view! {
         <Title text="Marko" />
-        <main>
-            <div class="h-screen bg-gray-900 text-gray-100">
+        <main class="overflow-hidden">
+            <div class="bg-gray-900 text-gray-100 h-[100svh] overflow-hidden">
                 <div class="h-full flex">
-                    <div class="flex-1 min-w-[200px] resize-x overflow-auto border-r border-gray-700 p-4">
+                    <div class="flex-1 min-w-[200px] resize-x overflow-hidden border-r border-gray-700 p-4">
                         <textarea
                             prop:value=move || input_value.get()
                             on:input:target=move |ev| set_values(ev.target().value())
@@ -64,13 +64,16 @@ fn Home() -> impl IntoView {
                             [&_p]:mb-4
                             [&_a]:text-teal-500 [&_a]:underline [&_a]:hover:text-teal-700
                             [&_strong]:font-semibold [&_strong]:text-gray-900
-                            [&_code]:bg-gray-100 [&_code]:px-2 [&_code]:py-1 [&_code]:rounded [&_code]:font-mono [&_code]:text-sm
-                            [&_pre]:bg-gray-100 [&_pre]:p-4 [&_pre]:rounded [&_pre]:overflow-x-auto
+                            [&_code]:bg-gray-800 [&_code]:px-2 [&_code]:py-1 [&_code]:rounded [&_code]:font-mono [&_code]:text-sm
+                            [&_pre]:bg-gray-800 [&_pre]:p-4 [&_pre]:rounded [&_pre]:overflow-x-auto
                             [&_pre>code]:bg-transparent [&_pre>code]:p-0
                             [&_ul]:list-disc [&_ul]:ml-6 [&_ul]:mb-4
                             [&_ol]:list-decimal [&_ol]:ml-6 [&_ol]:mb-4
                             [&_li]:mb-2
-                            [&_blockquote]:border-l-4 [&_blockquote]:border-gray-300 [&_blockquote]:pl-4 [&_blockquote]:italic [&_blockquote]:mb-4"
+                            [&_blockquote]:border-l-4 [&_blockquote]:border-gray-300 [&_blockquote]:pl-4 [&_blockquote]:italic [&_blockquote]:mb-4
+                            [&_pre]:bg-[#1E2022]
+                            overflow-auto
+                            "
                             inner_html=preview_value
                         ></div>
                     </div>
