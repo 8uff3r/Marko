9d06eb56a212793171a7bee28ad6edb8acf8eb78
8uff3r <8uff3r@gmail.com>
2025-03-02 14:43:17 +0330

    Add leptosfmt and format with it

--- src/app.rs
+++ src/app.rs
@@ -10,11 +10,11 @@ pub fn App() -> impl IntoView {
     provide_meta_context();
 
     view! {
-        <Stylesheet id="leptos" href="/style/output.css"/>
-        <Link rel="shortcut icon" type_="image/ico" href="/favicon.ico"/>
+        <Stylesheet id="leptos" href="/style/output.css" />
+        <Link rel="shortcut icon" type_="image/ico" href="/favicon.ico" />
         <Router>
             <Routes fallback=|| "Page not found.">
-                <Route path=StaticSegment("") view=Home/>
+                <Route path=StaticSegment("") view=Home />
             </Routes>
         </Router>
     }
@@ -26,11 +26,14 @@ fn Home() -> impl IntoView {
 
     // thanks to https://tailwindcomponents.com/component/blue-buttons-example for the showcase layout
     view! {
-        <Title text="Leptos + Tailwindcss"/>
+        <Title text="Leptos + Tailwindcss" />
         <main>
             <div class="bg-gradient-to-tl from-blue-800 to-blue-500 text-white font-mono flex flex-col min-h-screen">
                 <div class="flex flex-row-reverse flex-wrap m-auto">
-                    <button on:click=move |_| set_value.update(|value| *value += 1) class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white">
+                    <button
+                        on:click=move |_| set_value.update(|value| *value += 1)
+                        class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white"
+                    >
                         "+"
                     </button>
                     <button class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900 text-white">
@@ -39,7 +42,7 @@ fn Home() -> impl IntoView {
                     <button
                         on:click=move |_| set_value.update(|value| *value -= 1)
                         class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white"
-                        class:invisible=move || {value.get() < 1}
+                        class:invisible=move || { value.get() < 1 }
                     >
                         "-"
                     </button>
